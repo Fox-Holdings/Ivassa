c16e6d8ccf6f10d2ee41f7d471229c41ab57ef98
jemmiebugs <jemmynat@yahoo.com>
2022-02-11 08:51:28 +0100

    update

--- assets/inc/sendemail.php
+++ assets/inc/sendemail.php
@@ -1,46 +1,56 @@
 <?php
 
 // Define some constants
-define( "RECIPIENT_NAME", "Leo" );
-define( "RECIPIENT_EMAIL", "oyindamolamuritala@gmail.com" );
+define("RECIPIENT_NAME", "Leo");
+define("RECIPIENT_EMAIL", "oyindamolamuritala@gmail.com");
 
 // Read the form values
 $success = false;
-$name = isset( $_POST['name'] ) ? preg_replace( "/[^\.\-\' a-zA-Z0-9]/", "", $_POST['name'] ) : "";
-$senderEmail = isset( $_POST['email'] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['email'] ) : "";
-$phone = isset( $_POST['phone'] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['phone'] ) : "";
-$services = isset( $_POST['services'] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['services'] ) : "";
-$subject = isset( $_POST['subject'] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['subject'] ) : "";
-$address = isset( $_POST['address'] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['address'] ) : "";
-$discuss = isset( $_POST['discuss'] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['website'] ) : "";
-$message = isset( $_POST['message'] ) ? preg_replace( "/(From:|To:|BCC:|CC:|Subject:|Content-Type:)/", "", $_POST['message'] ) : "";
+$name = isset($_POST['name']) ? preg_replace("/[^\.\-\' a-zA-Z0-9]/", "", $_POST['name']) : "";
+$senderEmail = isset($_POST['email']) ? preg_replace("/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['email']) : "";
+$phone = isset($_POST['phone']) ? preg_replace("/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['phone']) : "";
+$services = isset($_POST['services']) ? preg_replace("/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['services']) : "";
+$subject = isset($_POST['subject']) ? preg_replace("/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['subject']) : "";
+$address = isset($_POST['address']) ? preg_replace("/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['address']) : "";
+$discuss = isset($_POST['discuss']) ? preg_replace("/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['discuss']) : "";
+$message = isset($_POST['message']) ? preg_replace("/(From:|To:|BCC:|CC:|Subject:|Content-Type:)/", "", $_POST['message']) : "";
 
 $mail_subject = 'A contact request send by ' . $name;
 
-$body = 'Name: '. $name . "\r\n";
-$body .= 'Email: '. $senderEmail . "\r\n";
+$body = 'Name: ' . $name . "\r\n";
+$body .= 'Email: ' . $senderEmail . "\r\n";
 
 
-if ($phone) {$body .= 'Phone: '. $phone . "\r\n"; }
-if ($services) {$body .= 'Services: '. $services . "\r\n"; }
-if ($subject) {$body .= 'Subject: '. $subject . "\r\n"; }
-if ($address) {$body .= 'Address: '. $address . "\r\n"; }
-if ($discuss) {$body .= 'Discuss: '. $discuss . "\r\n"; }
-if ($city) {$body .= 'City: '. $city . "\r\n"; }
-if ($store) {$body .= 'Store: '. $store . "\r\n"; }
-
+if ($phone) {
+  $body .= 'Phone: ' . $phone . "\r\n";
+}
+if ($services) {
+  $body .= 'Services: ' . $services . "\r\n";
+}
+if ($subject) {
+  $body .= 'Subject: ' . $subject . "\r\n";
+}
+if ($address) {
+  $body .= 'Address: ' . $address . "\r\n";
+}
+if ($discuss) {
+  $body .= 'Discuss: ' . $discuss . "\r\n";
+}
 $body .= 'message: ' . "\r\n" . $message;
-
+// if ($city) {
+//   $body .= 'City: ' . $city . "\r\n";
+// }
+// if ($store) {
+//   $body .= 'Store: ' . $store . "\r\n";
+// }
 
 
 // If all values exist, send the email
-if ( $name && $senderEmail && $message ) {
+if ($name && $senderEmail && $message) {
   $recipient = RECIPIENT_NAME . " <" . RECIPIENT_EMAIL . ">";
-  $headers = "From: " . $name . " <" . $senderEmail . ">";  
-  $success = mail( $recipient, $mail_subject, $body, $headers );
+  $headers = "From: " . $name . " <" . $senderEmail . ">";
+  $success = mail($recipient, $mail_subject, $body, $headers);
   echo "<div class='inner success'><p class='success'>Thanks for contacting us. We will contact you ASAP!</p></div><!-- /.inner -->";
-}else {
-	echo "<div class='inner error'><p class='error'>Something went wrong. Please try again.</p></div><!-- /.inner -->";
+} else {
+  echo "<div class='inner error'><p class='error'>Something went wrong. Please try again.</p></div><!-- /.inner -->";
 }
-
-?>
